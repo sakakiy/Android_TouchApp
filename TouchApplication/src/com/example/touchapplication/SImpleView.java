8378dedacd115fe9dd9eabe3ddb1234d56638329
榊原吉伸 <sakura.colonnade@gmail.com>
2014-11-16 01:39:09 +0900

    SimpleView を追加。

---
+++ TouchApplication/src/com/example/touchapplication/SImpleView.java
@@ -0,0 +1,19 @@
+package com.example.touchapplication;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class SImpleView extends SurfaceView implements Runnable {
+
+    public SImpleView(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
