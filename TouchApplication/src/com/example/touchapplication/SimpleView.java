11cc525dc983beb9efacbfa364025d61c7db002c
榊原吉伸 <sakura.colonnade@gmail.com>
2014-11-18 01:30:36 +0900

    複数点タッチして円が描画されるように変更
    - 円を 10 個まで表示
    - 終了時に NullPointerException でおちていたのを修正
      - holder.lockCanvas の返り値が NULL のまま canvas していてぬるぽっていたのでNULLチェックするように変更

--- TouchApplication/src/com/example/touchapplication/SimpleView.java
+++ TouchApplication/src/com/example/touchapplication/SimpleView.java
@@ -9,14 +9,19 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class SimpleView extends SurfaceView implements SurfaceHolder.Callback, Runnable{
+public class SimpleView extends SurfaceView implements SurfaceHolder.Callback,
+        Runnable {
 
-    private Paint  paint;
-    private Thread thread;
+    private Paint         paint;
+    private Thread        thread;
     private SurfaceHolder holder;
 
-    private float  x, y, rv;
-    private float  centerX, centerY;
+    final private int     NUM_MAX = 10;
+    private int           num     = 0;
+    private float[]       x       = new float[NUM_MAX];
+    private float[]       y       = new float[NUM_MAX];
+    private float         rv;
+    private float         centerX, centerY;
 
     public SimpleView(Context context) {
         super(context);
@@ -24,30 +29,45 @@ public class SimpleView extends SurfaceView implements SurfaceHolder.Callback, R
         thread = null;
         paint = new Paint();
 
-        x = y = 0;
+        for (int i = 0; i < NUM_MAX; i++) {
+            x[i] = 0;
+            y[i] = 0;
+        }
         centerX = centerY = 0;
         rv = 0;
         holder = getHolder();
         holder.addCallback(this);
-        
+
     }
 
     @Override
     public void run() {
         while (thread != null) {
+            try {
+                Thread.sleep(16);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             Canvas canvas = holder.lockCanvas();
+            if (canvas == null)
+                continue;
+
             canvas.drawColor(Color.BLACK);
 
             rv += 0.05;
-            x = (float) (centerX + 100 * Math.cos(rv));
-            y = (float) (centerY + 100 * Math.sin(rv));
+            // x = (float) (centerX + 100 * Math.cos(rv));
+            // y = (float) (centerY + 100 * Math.sin(rv));
 
             paint.setColor(Color.argb(255, 255, 255, 200));
             paint.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(x, y, 30, paint);
+
             paint.setColor(Color.argb(255, 255, 255, 255));
             paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(centerX, centerY, 100, paint);
+            // canvas.drawCircle(centerX, centerY, 100, paint);            
+            for (int i = 0; i < num; i++) {
+                canvas.drawCircle(x[i], y[i], 200, paint); 
+            }
 
             holder.unlockCanvasAndPost(canvas);
         }
@@ -68,6 +88,7 @@ public class SimpleView extends SurfaceView implements SurfaceHolder.Callback, R
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.v("surface destroyed", "surface destroyed");
         thread = null;
     }
 
@@ -75,10 +96,14 @@ public class SimpleView extends SurfaceView implements SurfaceHolder.Callback, R
     public boolean onTouchEvent(MotionEvent event) {
         centerX = event.getX();
         centerY = event.getY();
+        num = event.getPointerCount();
+        for(int i=0; i<num; i++){
+            x[i] = event.getX(i);
+            y[i] = event.getY(i);
+        }
 
         // Androidの仕様がちょっとバグい。true を返さず super.onTouchEvent(event) を返すと
         // false になっていて成立しなくなるらしい。
         return true;
     }
-
 }
